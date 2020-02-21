de3aec630018f591189e3d99ece7b0955b1b55e9
unknown <khyati11892@gmail.com>
2020-02-21 16:01:47 +0530

    first commit

---
+++ scrip.js
@@ -0,0 +1,13 @@
+// const hamburger = document.querySelector('#hamburger');
+ const navmenu = document.querySelector('.navMenu');
+
+// hamburger.addEventListener('click',() => {
+// 	navmenu.classList.toggle('show');
+	
+// });
+
+function myFunction(x) {
+  x.classList.toggle("change");
+  navmenu.classList.toggle('show');
+ 
+}
